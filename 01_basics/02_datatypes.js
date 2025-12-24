6a3ecccaa8cc44195ffa0622e598a1f3354907e8
honeyjariwala022 <honeyjariwala022@gmail.com>
2025-12-24 11:58:44 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+2) // We Are Using nodejs not browser
+
+console.log(3+3) //code readability should be high//
+
+console.log("Honey");
+
+let name = "Honey"
+
+let age = 18
+let isLoggedIn = false
+let state = null
+
+//number ==> 2 to the power of 53//
+//bigint==> it Is Use In Big Websites..//
+//string==>It IS use In 2 Cots("" double cots) & ('' singal cots) best is double cots ("")//
+//boolean==>true / false//
+//null==>standalone value//
+//undefined
+//symbol==>unique//
+
+//object
+
+console.log(typeof undefined);  //undefined
+console.log(typeof null);   //object
+
+
