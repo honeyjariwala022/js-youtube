b5dd7e69a2308a768d4e9936df117000ba569c4a
honeyjariwala022 <honeyjariwala022@gmail.com>
2025-12-24 12:33:29 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "33abcd"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" ==> 33//
+//"33abcd" ==> Na->not a number//
+//true ==> 1; false ==>0
+
+
+let isLoggedIn ="Honey"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1==> true; 0==>false//
+//"" ==> false//
+//"Honey" ==> True//
+
+
+let someNumber = 22
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
