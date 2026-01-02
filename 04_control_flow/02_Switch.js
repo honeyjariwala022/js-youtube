40f6f3c4b74abbae3fa3a4506457771c8ce1ba82
honeyjariwala022 <honeyjariwala022@gmail.com>
2026-01-02 10:30:42 +0000

    Switch Statments

---
+++ 04_control_flow/02_Switch.js
@@ -0,0 +1,34 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+//Key Is That Value That We Can Cheaked All Time//
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("Fab");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+
+    default:
+        console.log("default case Match");
+        break;
+}
+ 
